6b2068344e8ff2faa0de91d500e5ead97baddab3
mattresssalsa <matt.sousa@live.ca>
2026-05-19 13:02:48 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.AI_IDEAS_SUPABASE = {
+  url: "https://odjfkrmzwtbqfxsmfajg.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kamZrcm16d3RicWZ4c21mYWpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxOTQ3MjEsImV4cCI6MjA5NDc3MDcyMX0.sv67dSKMFxBMW9yna56d_7Og_fhkXevfUUZR_iP-iU0"
+};
